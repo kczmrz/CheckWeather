5ce98a00138d66bdcaa822962f605a837eed1183
kczmrz <kaacpersky00@gmail.com>
2022-07-11 17:03:43 +0200

    RepairFooter

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer()
    
     
     return(<>
-    <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800">
+    <footer className="p-4 bg-gray-800 shadow md:flex md:items-center md:justify-between md:p-6 ">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">2022 by Kczmrz
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-xl text-white sm:mt-0">
